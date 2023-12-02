fa69565def6765e09f092f367781265c5ac02cfb
kenkalang <kenkalang@yahoo.com>
2023-12-02 21:07:03 +0700

    fix: user data auth

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('./middlewares/cors');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const authController = require('./controllers/auth-controller');
 const profileController = require('./controllers/profile-controller');
 
@@ -15,6 +16,7 @@ async function setupRoutes(app) {
         const app = express();
         app.use(express.json());
         app.use(cors);
+        app.use(morgan('combined'));
         app.use(bodyParser.urlencoded({ extended: true }));
         await setupRoutes(app);
         app.listen(process.env.PORT, () => {
