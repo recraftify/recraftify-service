c8fe44e14d4d53b2b3a0276584d21a0ea0040e51
kenkalang <kenkalang@yahoo.com>
2023-12-09 00:52:05 +0700

    fix: port option

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ async function setupRoutes(app) {
         app.use(morgan('combined'));
         app.use(bodyParser.urlencoded({ extended: true }));
         await setupRoutes(app);
-        app.listen(PORT, '0.0.0.0', () => {
+        app.listen(PORT || 8080, '0.0.0.0', () => {
             console.log(`recraftify-app listening at Port ${process.env.PORT}`);
         });
     } catch (error) {
