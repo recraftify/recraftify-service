923e992c50895c6c4234b7b8ac797bde5c8badf3
kenkalang <kenkalang@yahoo.com>
2023-12-11 18:54:20 +0700

    fix: service account path

--- src/clients/google-firestore-admin/index.js
+++ src/clients/google-firestore-admin/index.js
@@ -1,10 +1,11 @@
 var admin = require('firebase-admin');
 var { getFirestore } = require('firebase-admin/firestore');
+const serviceAccount = 'gs://recraftify-service.appspot.com/keys.json';
 
 function getDB() {
     if (!admin.apps.length) {
         admin.initializeApp({
-            credential: admin.credential.cert(process.env.FIREBASE_SERVICE_KEY),
+            credential: admin.credential.cert(serviceAccount),
         });
     }
     return getFirestore();
