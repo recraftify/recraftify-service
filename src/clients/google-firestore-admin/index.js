9cb689968c2f176cd76816443b140665d03a3917
kenkalang <kenkalang@yahoo.com>
2023-12-11 18:42:23 +0700

    fix: firebase-auth

--- src/clients/google-firestore-admin/index.js
+++ src/clients/google-firestore-admin/index.js
@@ -2,10 +2,9 @@ var admin = require('firebase-admin');
 var { getFirestore } = require('firebase-admin/firestore');
 
 function getDB() {
-    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_KEY);
     if (!admin.apps.length) {
         admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount),
+            credential: admin.credential.cert(process.env.FIREBASE_SERVICE_KEY),
         });
     }
     return getFirestore();
