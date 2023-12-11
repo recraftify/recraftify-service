31d9dcd5d6205d56e003c643efcac20ae971ba84
kenkalang <kenkalang@yahoo.com>
2023-12-11 18:54:20 +0700

    fix: service account path

--- src/clients/google-cloud-storage/connector.js
+++ src/clients/google-cloud-storage/connector.js
@@ -1,13 +1,14 @@
 const Cloud = require('@google-cloud/storage');
 const StandardError = require('../../utils/standard-error');
 const { GCP_PROJECT_NAME, GCP_BUCKET_NAME } = process.env;
+const serviceAccount = 'gs://recraftify-service.appspot.com/keys.json';
 const { Storage } = Cloud;
 
 async function getBucket() {
     let bucket;
     try {
         const storage = new Storage({
-            keyFilename: process.env.FIREBASE_SERVICE_KEY,
+            keyFilename: serviceAccount,
             projectId: GCP_PROJECT_NAME,
         });
 
