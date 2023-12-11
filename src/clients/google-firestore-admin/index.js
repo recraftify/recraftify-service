0a62f115b601722927ff6c58a5cc2060bf2c6930
kenkalang <kenkalang@yahoo.com>
2023-12-11 17:59:47 +0700

    fix: service-account usage

--- src/clients/google-firestore-admin/index.js
+++ src/clients/google-firestore-admin/index.js
@@ -5,8 +5,12 @@ const { SERVICE_KEY } = process.secrets;
 
 function getDB() {
     if (!admin.apps.length) {
+        const serviceAccount =
+            typeof SERVICE_KEY === 'string'
+                ? JSON.parse(SERVICE_KEY)
+                : SERVICE_KEY;
         admin.initializeApp({
-            credential: admin.credential.cert(SERVICE_KEY),
+            credential: admin.credential.cert(serviceAccount),
         });
     }
     return getFirestore();
